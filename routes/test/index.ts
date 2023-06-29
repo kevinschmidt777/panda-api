1b2b258f5ed7dbe8b97abef0083901c75248e8c8
Kevin Schmidt <kevin@kswebentwicklung.de>
2023-06-29 16:43:26 +0200

    comment

--- routes/test/index.ts
+++ routes/test/index.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance, FastifyPluginAsync } from "fastify";
 import { IRouteGetTestParams, IRouteGetTestQuerystring } from "./schema";
 
+/**
+ * Panda API uses a index.ts file in each route folder to register the routes for that folder.
+ * Below you can see an example of how to register a simple get route, including the schema.
+ * The seperate schema file is used to define the types of the routes.
+ */
+
 const testRoutes: FastifyPluginAsync = async (server: FastifyInstance) => {
   server.get<{
     Querystring: IRouteGetTestQuerystring;
